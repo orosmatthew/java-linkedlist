8a8e22a3ba1caaab31c6ecc4af92618ef7ece97c
orosmatthew <orosmatthew@pm.me>
2021-09-21 09:34:24 -0400

    removed dummy main

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,8 @@
 public class Main {
 
 	public static void main(String[] args) {
-		LinkedListNode<Integer> testNode = new LinkedListNode<Integer>(5);
+
 		
-		System.out.println(testNode.getData());
 	}
 
 }
